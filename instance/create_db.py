62e16b9cde691026240117b17f101da3aa37cb34
iml8271 <iml@clemson.edu>
2026-02-04 12:43:55 -0500

    created db for all users
    diff tables for drivers, sponsors, admins, etc.

---
+++ instance/create_db.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+conn = sqlite3.connect('safeDrivingApp.db')
+cursor = conn.cursor()
+cursor.execute("PRAGMA foreign_keys = ON")
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS sponsors (
+    sponsor_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    sponsor_name TEXT NOT NULL,
+    contact_email TEXT
+)
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS drivers (
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    driver_fname TEXT NOT NULL,
+    driver_lname TEXT NOT NULL,
+    driver_points INTEGER DEFAULT 0,
+    sponsor_id INTEGER,
+    FOREIGN KEY (sponsor_id) REFERENCES sponsors(sponsor_id) ON DELETE SET NULL
+)
+''')
+
+conn.commit()
+conn.close()
+print("Database created successfully!")
